3153442c402546b6829cb673932e8fcad8a2c45a
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-09-21 17:52:32 -0500

    PaceCalculator complete

--- PaceCalculator.py
+++ PaceCalculator.py
@@ -2,10 +2,18 @@
 # - Distance 21.01 km
 # - Pace 7:49/km
 # - Time 2h 44m
+# 
+# ### Another example:
+# - Distance 6.44 km
+# - Pace 7:20/km
+# - Time 47m 16s
+# 
+# This is something similar to the end product:
+# https://www.xconvert.com/unit-converter/minutes-per-kilometre-to-minutes-per-mile
 
 try:
     # Asking user for this time
-    time = input('Enter your time in the following format: hours, minutes, seconds')
+    time = input('Enter your time in the following format: hours, minutes, seconds:')
     time = time.split(',')
     
     # From the input, getting the hours, minutes and seconds
@@ -14,35 +22,61 @@ try:
     seconds = float(time[2])
     
 except:
-    print('Please enter time in the correct format: h,m,s')
+    print('Please enter time in the correct format: h,m,s:')
 
-distance = float(input('What distance did you run? Format: 0.0'))
-format = input('Are you measuring in kilometers or miles? Please enter: km or m')
+distance = float(input('What distance did you run? Format: 0.0:'))
+format = input('Are you measuring in kilometers or miles? Please enter: km or m:')
 
+if hours > 0:
+    # Converting seconds to decimals
+    seconds = seconds/100
 
-# Converting seconds to decimals
-seconds = seconds/100
+    # Convert hours to minutes
+    hours = 2 
+    hours2minutes = hours * 60
+    minutes = hours2minutes + minutes
 
-# Convert hours to minutes
-hours = 2 
-hours2minutes = hours * 60
-minutes = hours2minutes + minutes
+    # Calculating pace
+    pace = minutes/distance
+    conversion_pace = pace
 
+    # Converting pace decimals into actual minutes
+    pace_decimals = pace % 1
+    pace_decimals = (pace_decimals * 60)/100
 
-# Calculating pace
-pace = minutes/distance
-conversion_pace = pace
+    pace = round(pace - (pace%1) + pace_decimals,2)
+    
+# Need to create code to handle when 0,x,x
+else:
+    # Converting seconds to decimals
+    seconds = seconds/100
+    minutes = minutes + seconds
+    
+    # Calculating pace
+    pace = minutes/distance
+    conversion_pace = pace
 
-# Converting pace decimals into actual minutes
-pace_decimals = pace % 1
-pace_decimals = (pace_decimals * 60)/100
+    # Converting pace decimals into actual minutes 
+    pace_decimals = pace % 1
+    pace_decimals = (pace_decimals * 60)/100
 
-pace = round(pace - (pace%1) + pace_decimals,2)
-
+    pace = round(pace - (pace%1) + pace_decimals)
 
 # Printing pace
 print(f'Your was: {pace}/{format}')
 
+conversion_pace
 
 # Converting pace to the opposite format. i.e. km -> m or m -> km
-conversion_pace
+if format == 'km' or 'kilometer' or 'k':
+    pace_in_miles = pace * 1.6093491499172796
+    print('Your pace in miles is:',round(pace_in_miles,3))
+
+elif format == 'mi' or 'miles' or 'm':
+    pace_in_kilometers = pace / 1.6093491499172796
+    print('Your pace in kilometers is:',round(pace_in_kilometers,3))
+
+
+
+
+
