ccf15c2feb6dc661e7538f5e0490eb0bba331870
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-09-23 14:36:58 -0500

    Added text box input

--- WebAppTest.py
+++ WebAppTest.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="Pace Converter", page_icon="Resources\RunningIcon_Test.jpg", layout="wide")
 
-# Header for
+# Header
 st.subheader("Pace Converter")
+
+# Drop box -> selection if km or m
 st.selectbox('What was your format, km or mi?', options=["Kilometers","Miles"])
+
+# Enter pace here
+title = st.text_input("What was your pace?")
+
+st.write('Your pace is:', title)
