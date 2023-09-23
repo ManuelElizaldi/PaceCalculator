b15e0e2f0c5d36c34294fd1f4b29bb9d80f9e86c
Manuel Elizaldi <manuelelizaldib@gmail.com>
2023-09-23 14:31:05 -0500

    Added dropbox option

--- WebAppTest.py
+++ WebAppTest.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="Pace Converter", page_icon=":tada:", layout="wide")
+st.set_page_config(page_title="Pace Converter", page_icon="Resources\RunningIcon_Test.jpg", layout="wide")
 
 # Header for
 st.subheader("Pace Converter")
-st.selectbox('What was your format, km or mi?')
+st.selectbox('What was your format, km or mi?', options=["Kilometers","Miles"])
